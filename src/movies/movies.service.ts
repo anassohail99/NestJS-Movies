d1d45b3af0cfc9daa055b6fee96a86180c762360
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-16 00:38:59 +0500

    Movies Services

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,45 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { MOVIES } from './movies.mock';
+
+@Injectable()
+export class MoviesService {
+    movies = MOVIES; 
+
+    getMovies(): Promise<any>{
+        return new Promise((resolve)=>{
+            resolve(this.movies);
+        })
+    }
+
+    getMovie(movieID): Promise<any>{
+        let id = Number(movieID);
+        return new Promise((resolve)=>{
+            const movieTemp = this.movies.find(movie => movie.id === id);
+            if(!movieTemp){
+                throw new HttpException('Movie doesnot exitst!',404);
+            }
+            resolve(movieTemp)
+        })
+    }
+
+
+    addMovie(movie): Promise<any>{
+        return new Promise((resolve)=>{
+            this.movies.push(movie);
+            resolve(this.movies);
+        })
+    }
+
+
+    deleteMovie(movieID): Promise<any>{
+        let id = Number(movieID);
+        return new Promise((resolve)=>{
+            let index  = this.movies.findIndex(movie => movie.id === id);
+            if(index === -1 ){
+                throw new HttpException("No such movie found", 404);
+            }
+            this.movies.splice(index, 1);
+            resolve(this.movies)
+        })
+    }
+}
