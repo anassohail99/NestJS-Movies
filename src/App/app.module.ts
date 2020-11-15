f51a653f2b4a165895a151f7890e299ee6cf01f2
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-16 00:38:35 +0500

    Directory Change

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,7 +1,8 @@
+import { MoviesModule } from './../movies/movies.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesModule } from './movies/movies.module';
+// import { MoviesModule } from '../movies/movies.module';
 
 @Module({
   imports: [MoviesModule],
