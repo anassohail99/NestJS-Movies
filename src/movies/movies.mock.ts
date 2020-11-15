cc042d121664043dc92c1eb554c23bdbbc25a3ef
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-16 00:40:12 +0500

    Movies array

---
+++ src/movies/movies.mock.ts
@@ -0,0 +1,7 @@
+import { title } from "process"
+
+export const MOVIES =[
+    {id:1 , title:"Black Panther" , director:"Ayesha Ahmed"},
+    {id:2 , title:"Captain America" , director:"Ismail Lodhi"},
+    {id:3 , title:"Jhon Wick" , director:"Anas Sohail"}
+]
